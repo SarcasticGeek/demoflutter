9e410e445658665761531c69db503ce6bba93f1b
Mohamed Essam <mohamedessamfathalla@gmail.com>
2018-12-17 01:07:34 +0200

    1st commit

---
+++ lib/src/app.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'dart:convert';
+import 'models/imageModel.dart';
+import 'widgets/imageList.dart';
+
+class App extends StatefulWidget {
+  createState() {
+    return AppState();
+  }
+}
+class AppState extends State<App> {
+  int counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    counter++;
+    var response = await get('https://jsonplaceholder.typicode.com/photos/$counter');
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
+    
+    setState(() {
+      images.add(imageModel);
+    });
+    
+  }
+
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ImageList(images),
+        floatingActionButton: FloatingActionButton(
+          onPressed: fetchImage,
+          child: Icon(
+            Icons.add
+          ),
+        ),
+        appBar: AppBar(
+          title: Text('Hey'),
+        ),
+      ),
+    );
+  }
+}
